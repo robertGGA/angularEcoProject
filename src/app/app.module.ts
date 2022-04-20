b4993b23ef7e8f523540d1b4e27bc64631fadd38
robert <78978033+robertGGA@users.noreply.github.com>
2022-04-20 14:43:17 +0300

    added half of header and some utils

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from '@pages/main-page/main-page.component';
+import { HeaderComponent } from '@components/header/header.component';
+import {IconComponent} from '@components/ui/icon/icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/ui/icon/icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IconComponent {
+	width!: number;
+	height!: number
+
+  constructor() {
+  }
+}
